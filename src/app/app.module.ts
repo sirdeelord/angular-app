0ac4da055063eefc65ba479f23cb971041283773
Saddy <saddyodus@yahoo.com>
2017-12-27 21:53:44 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
